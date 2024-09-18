7af03eaa57afa81069bf9267a161845718b9c29f
kkplak <kkplak@gmail.com>
2024-09-18 11:41:35 +0200

    ready for deploy

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -8,12 +8,19 @@ function Home() {
   return (
     <div className="wrapper">
       <div className="text-container home">
-        <p>
+        {/* <p>
           Hello, I'm a frontend developer with a love for great design and fun,
           interactive websites. In my day-to-day work, I collaborate with
           cross-functional teams to build websites and web games for renowned
           clients such as Marvel, Disney, Universal Studios, Warner Bros, and
           McDonald's.
+        </p> */}
+        <p>
+          Hello, I'm a frontend developer with a passion for creating visually
+          stunning and interactive websites. I've had the pleasure of working
+          with renowned clients like Marvel, Disney, Universal Studios, Warner
+          Bros, and McDonald's, bringing their visions to life through engaging
+          web experiences.
         </p>
       </div>
     </div>
